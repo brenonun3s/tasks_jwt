ccd0effc9c1f7cc13dffd040bd75e3db7d8469d1
brenonun3s <morais.brenonunes@hotmail.com>
2025-07-10 10:23:59 -0300

    Iniciado documentação das API's no AuthController e UserRegisterDTO

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -11,17 +11,36 @@ import com.example.demo.dto.UserRegisterDTO;
 import com.example.demo.dto.UserResponseDTO;
 import com.example.demo.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Autorização de Usuários", description = "Operações relacionadas aos usuários e obtenção do token JWT")
 public class AuthController {
 
  private final UserService userService;
  
+
  @PostMapping("/register")
+ @Operation(summary = "Usuário se registrar", description = "Usuário envia seu nome, email e senha para se registrar e obter o token JWT")
+ @ApiResponses({
+  @ApiResponse(responseCode = "200", description = "Usuário cadastrado com sucesso!"),
+  @ApiResponse(responseCode = "400", description = "Campo obrigatório não preenchido"),
+  @ApiResponse(responseCode = "409", description = "Email já em uso!")
+ })
+ @io.swagger.v3.oas.annotations.parameters.RequestBody(
+  description = "Dados para registrar um novo usuário",
+  required = true,
+  content = @Content(schema = @Schema(implementation = UserRegisterDTO.class))
+ )
  public UserResponseDTO register(@RequestBody @Valid UserRegisterDTO dto) {
   return userService.register(dto);
  }
--- src/main/java/com/example/demo/dto/UserRegisterDTO.java
+++ src/main/java/com/example/demo/dto/UserRegisterDTO.java
@@ -1,19 +1,23 @@
 package com.example.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRegisterDTO(
 
+    @Schema(description = "Nome do usuário", example = "Breno Nunes")
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     String name,
 
+    @Schema(description = "Email do usuário", example = "breno@gmail.com")
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
     String email,
 
+    @Schema(description = "Senha do usuário", example = "123456")
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     String password
